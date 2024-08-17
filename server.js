56df2ee04251e6ff861c2667ad024fca9796d370
Zain Ul Abid <bajwazain121@outlook.com>
2024-08-17 13:48:45 -0400

    Setup front end with transaction js

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-const express = require("express");
-const session = require("express-session");
-const sequelize = require("./config/connection");
-const authRoutes = require("./routes/auth");
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const db = require("./models");
 
+const db = require('./models');
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 //Middleware
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Sync database
 db.sequelize.sync({ force: false }).then(() => {
-  console.log("Database connected");
+  console.log('Database connected');
 });
 
 //session middleware
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes);
 app.use(
   session({
-    secret: "MySuperDooperSecretKey",
+    secret: 'MySuperDooperSecretKey',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }, // Set to true if using HTTPS
@@ -30,14 +36,17 @@ app.use(
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
   })
   .catch((err) => {
-    console.error("Unable to connect to the database:", err);
+    console.error('Unable to connect to the database:', err);
   });
 
 //Routes
-app.get("");
+app.get('');
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
