226c6a8ad78f027508cd3df090294c53e620e8c0
Zain Ul Abid <bajwazain121@outlook.com>
2024-08-27 18:48:45 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,17 @@ const { Sequelize } = require('sequelize');
 const db = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-
-const sequelize = new Sequelize(db, dbUser, dbPassword, {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;
+let sequelize;
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
