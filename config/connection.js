9a6e619fcd252afd5f031ac2e8bfcf97565b0c1f
elissegomez24 <elissegomez24@gmail.com>
2024-08-26 21:09:41 -0400

    updated file changes

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
-const { Sequelize } = require('sequelize');
-<<<<<<< HEAD
-const sequelize = new Sequelize('expensewise', 'postgres', 'Elisse24!', {
-=======
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
 const db = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
 const sequelize = new Sequelize(db, dbUser, dbPassword, {
->>>>>>> 1764c7238b89927a1425ca4623e69adc0af51a08
   host: 'localhost',
   dialect: 'postgres',
 });
+
 module.exports = sequelize;
