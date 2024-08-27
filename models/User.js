7f3c3e2350f088649e7df23a8725d712df0fc256
Zain Ul Abid <bajwazain121@outlook.com>
2024-08-27 18:56:14 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize');
+const { Sequelize,DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcryptjs');
 
